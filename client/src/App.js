fe46c6f936721b939f8acbdc35546dafdd2a4e08
Farhana Bente Islam <21303195@iubat.edu>
2025-04-16 09:55:15 +0600

    add neccessary app.css/app.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+// Components
+import HomeLayout from "./components/layout/HomeLayout";
+
+function App() {
+  return (
+    <HomeLayout />
+  );
+}
+
+export default App;

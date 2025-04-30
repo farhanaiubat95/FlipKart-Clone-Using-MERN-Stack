cbd9fea06f21f08abc56ba98a0356d0a429a600e
Farhana Bente Islam <21303195@iubat.edu>
2025-04-30 09:14:07 +0600

    added app.js

---
+++ server/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+// import DefaultData from './default.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+// import routes
+// import UserRouter from './routes/userRoute.js';
+import productRouter from './routes/productsRoute.js';
+import paymentRouter from './routes/paymentRouter.js';
+import Authrouters from './routes/AuthRouter.js';
+import Adminrouter from './routes/AdminRouter.js';
+
+// Initialize express
+const app = express();
+
+// Initialize cookie-parser
+app.use(cookieParser());
+
+// Initialize Default product Data
+// DefaultData();
+
+// use cors
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
+
+
+// use body-parser
+ app.use(bodyParser.json({ extended: true })); // to show the body which fill up or post to the server
+ app.use(bodyParser.urlencoded({ extended: true })); // handle URL space
+
+
+// User Login & register
+app.use('/', Authrouters)
+app.use('/admin', Adminrouter)
+app.use('/',productRouter);
+app.use('/',paymentRouter);
+
+export default app;

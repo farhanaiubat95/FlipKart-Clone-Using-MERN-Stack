5b67287f8c669285938f80e7ca6beb706e68dd29
Farhana Bente Islam <21303195@iubat.edu>
2025-05-01 03:30:47 +0600

    update router

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import {Signup, Login, Logout} from '../controller/AuthController.js';
+import {Signup, Login, Logout, VerifyEmail} from '../controller/AuthController.js';
 
 const Authrouters = express.Router();
 
 Authrouters.post('/signup', Signup);
 Authrouters.post('/login', Login);
 Authrouters.post('/logout', Logout);
+
+Authrouters.post('/verifyemail', VerifyEmail);
 
 export default Authrouters;

5c7d6fef8d1e1a7051c339ffb07dc88899d39212
Farhana Bente Islam <21303195@iubat.edu>
2025-05-02 02:53:12 +0600

    Updatee admin controller

--- server/routes/AdminRouter.js
+++ server/routes/AdminRouter.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { Getuser , DeleteUser} from '../controller/Admincontroller.js';
+import { Getuser , DeleteUser, ToggleUserActivity, GetUserById,UpdateUser, GetUserStats} from '../controller/Admincontroller.js';
 import { isAdmin } from '../middleware/VerifyToken.js';
 
 const Adminrouter = express.Router();
 
 Adminrouter.get('/dashboard/getuser', isAdmin , Getuser)
-Adminrouter.post('/delete/:id', isAdmin , DeleteUser)
+Adminrouter.get('/dashboard/getuserstats', isAdmin , GetUserStats)
+Adminrouter.get('/dashboard/getuserbyid/:id', isAdmin, GetUserById);
+Adminrouter.post('/dashboard/updateuser/:id', isAdmin, UpdateUser);
+Adminrouter.post('/dashboard/delete/:id', isAdmin , DeleteUser)
+Adminrouter.post('/dashboard/toggle-activity/:id', isAdmin, ToggleUserActivity)
 
 export default Adminrouter;
